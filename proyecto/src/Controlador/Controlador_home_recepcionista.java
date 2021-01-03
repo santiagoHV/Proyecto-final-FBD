b291025926fc04e9b8ccefbfe97f660611a8928f
santiagoHV <santiherreravelas@gmail.com>
2021-01-03 13:26:25 -0500

    edicion en la vista de recepcionista reserva y agregadas plantillas basicas para las demas vistas del recepcionista

--- proyecto/src/Controlador/Controlador_home_recepcionista.java
+++ proyecto/src/Controlador/Controlador_home_recepcionista.java
@@ -1,8 +1,17 @@
 package Controlador;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class Controlador_home_recepcionista {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controlador_home_recepcionista implements Initializable {
 
     public Button home_button;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
