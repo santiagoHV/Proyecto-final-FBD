43ddf1680a69312447e28716b69115ac31d016b9
santiagoHV <santiherreravelas@gmail.com>
2021-01-07 22:09:48 -0500

    agregado controlador de registros

---
+++ proyecto/src/Controlador/Controlador_datos_ingreso.java
@@ -0,0 +1,13 @@
+package Controlador;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controlador_datos_ingreso implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
