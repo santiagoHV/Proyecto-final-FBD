b27da8398c7f3cfb5c3d6b549e1aac3c38b83c72
ElHurta <judhurtadop@correo.udistrital.edu.co>
2021-01-30 20:37:09 -0500

    Agregada funcionalidad de búsqueda de reservas en Search

--- proyecto/src/Modelo/entidades/Reserva.java
+++ proyecto/src/Modelo/entidades/Reserva.java
@@ -5,6 +5,8 @@ import DatosSQL.Operaciones;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Reserva {
     private int k_reserva;
@@ -123,6 +125,45 @@ public class Reserva {
             reserva.setPersona(persona.ConsultarPersona(resultSet.getInt(9),resultSet.getString(10),persona));
 
             return reserva;
+
+        }catch (SQLException ex){
+            System.out.println(ex);
+        }
+        return null;
+    }
+
+    public List<Reserva> BuscarReservas(int k_reserva, int num_doc, String nom_or_apel) {
+
+        List<Reserva> reservaList = new ArrayList<>();
+
+        Operaciones op = new Operaciones();
+        try {
+            ResultSet resultSet =
+                    op.ConsultaEsp("SELECT * FROM Reserva WHERE k_reserva = "+k_reserva+" or k_identificacion = "+num_doc+" or " +
+                            "k_identificacion = (SELECT k_identificacion FROM Persona WHERE n_nombre IN ('"+nom_or_apel+"') or n_apellido IN ('"+nom_or_apel+"')) ");
+
+            Reserva reserva;
+            while (resultSet.next()) {
+
+                reserva = new Reserva();
+                reserva.setK_reserva(resultSet.getInt(1));
+                reserva.setEstado(resultSet.getString(2));
+                reserva.setF_inicio(resultSet.getDate(3));
+                reserva.setF_reserva(resultSet.getDate(4));
+                reserva.setF_final(resultSet.getDate(5));
+                reserva.setCantidad_huespedes(resultSet.getInt(6));
+                reserva.setPrecio_reserva(resultSet.getDouble(7));
+
+                Condicion_Hotel condicion_hotel = new Condicion_Hotel();
+                reserva.setCondicion(condicion_hotel.ConsultarCondicionHotel(resultSet.getInt(8),condicion_hotel));
+
+                Persona persona = new Persona();
+                reserva.setPersona(persona.ConsultarPersona(resultSet.getInt(9),resultSet.getString(10),persona));
+
+                reservaList.add(reserva);
+            }
+
+            return reservaList;
 
         }catch (SQLException ex){
             System.out.println(ex);
